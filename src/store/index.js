0e30b4927cbd85dd8d34a778721fda44e1d478a1
Qccccc's laptop <896853205@qq.com>
2018-01-09 15:19:37 +0800

    项目依赖初始化

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+// 使用Vuex
+Vue.use(Vuex)
+
+// vuex配置
+let store = new Vuex({
+  state: {},
+  mutations: {},
+  actions: {}
+})
+
+export default store
